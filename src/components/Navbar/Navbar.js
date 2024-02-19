8423d7832157ed05535688c0520bfb11875ca0e9
Abdur-P-Rahim <bspirahim@gmail.com>
2024-02-19 22:59:02 +0600

    initial setup and navbar added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='bg-primary shadow-xl drop-shadow-xl'>
+            <div className="navbar z-10 text-white max-w-screen-lg mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a href="">home</a></li>
+                        </ul>
+                    </div>
+                    <a className="text-xl font-bold cursor-pointer">BookFlow</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 text-md">
+                    <li><a href="">home</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <Link href='login'>Login</Link>/
+                    <Link href='register'>Register</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,31 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#20c997",
+
+          "secondary": "#f000b8",
+
+          "accent": "#1dcdbc",
+
+          "neutral": "#2b3440",
+
+          "base-100": "#ffffff",
+
+          "info": "#3abff8",
+
+          "success": "#36d399",
+
+          "warning": "#fbbd23",
+
+          "error": "#f87272",
+        },
       },
-    },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
